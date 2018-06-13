1b981b638d212b72bbcf04f7f509e39c99521497
Kaciano Ghelere <themohawkeagle@gmail.com>
2018-06-13 09:14:14 -0300

    exemplo se jsx adicionado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MyComponent = (props) => {
+  return React.createElement('div', null, `${props.title}`);
+};
+
+ReactDOM.render(
+  React.createElement(MyComponent, {title: 'Teste'}, null),
+  document.getElementById('root')
+);
 
 registerServiceWorker();
